1ee6ba8c0e10b10b7a534a2da5bac8f332aca99d
Anwar659 <contact.tariqanwar@gmail.com>
2024-08-12 01:43:15 +0530

    mailphp uodate

--- contact.php
+++ contact.php
@@ -7,9 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars(trim($_POST["message"]));
 
     // Email settings
-    $to = "mailto:contact.tariqanwar@gmail.com";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
+    $to = "contact.tariqanwar@gmail.com";
+    $headers = "From: $email" . "\r\n";
+    $headers .= "Reply-To: $email" . "\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
     
     // Email content
